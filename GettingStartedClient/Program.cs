61defbc028ee2ef6bc5a1a329499efe83654451e
Ngounou, Asty Noukeu <AstyNoukeu.Ngounou@cellent.de>
2018-07-09 14:15:02 +0200

    projects added

---
+++ GettingStartedClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using GettingStartedLib;
+using System;
+
+namespace GettingStartedClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = ServiceClient<ICalculatorService>.GetService();
+            var res = svc.Add(1, 2);
+            //Step 1: Create an instance of the WCF proxy.  
+            //CalculatorServiceClient client = new CalculatorServiceClient();
+
+            // Step 2: Call the service operations.  
+            // Call the Add service operation.  
+            double value1 = 100.00D;
+            double value2 = 15.99D;
+            double result = ServiceClient<ICalculatorService>.Execute(o => o.Add(value1, value2));
+            //client.Add(value1, value2);
+            Console.WriteLine("Add({0};{1}) = {2}", value1, value2, result);
+
+            // Call the Subtract service operation.  
+            value1 = 145.00D;
+            value2 = 76.54D;
+            result = ServiceClient<ICalculatorService>.Execute(o => o.Subtract(value1, value2));
+            //client.Subtract(value1, value2);
+            Console.WriteLine("Subtract({0};{1}) = {2}", value1, value2, result);
+
+            // Call the Multiply service operation.  
+            value1 = 9.00D;
+            value2 = 81.25D;
+            result = ServiceClient<ICalculatorService>.Execute(o => o.Multiply(value1, value2));
+            //client.Multiply(value1, value2);
+            Console.WriteLine("Multiply({0};{1}) = {2}", value1, value2, result);
+
+            // Call the Divide service operation.  
+            value1 = 22.00D;
+            value2 = 7.00D;
+            result = ServiceClient<ICalculatorService>.Execute(o => o.Divide(value1, value2));
+            //client.Divide(value1, value2);
+            Console.WriteLine("Divide({0};{1}) = {2}", value1, value2, result);
+
+            Console.ReadLine();
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.  
+            //client.Close();
+        }
+    }
+}
---
+++ GettingStartedClient/ServiceClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace GettingStartedClient
+{
+    public static class ServiceClient<T> 
+    {
+        private static readonly ChannelFactory<T> ChannelFactory;
+
+        private static readonly string EndpointAddress = "http://localhost:8733/Design_Time_Addresses/GettingStartedLib/CalculatorService/CalculatorService";
+        static ServiceClient()
+        {
+            WSHttpBinding wSHttpBinding = new WSHttpBinding();
+            EndpointAddress myEndpoint = new EndpointAddress(EndpointAddress);
+            ChannelFactory = new ChannelFactory<T>(wSHttpBinding, myEndpoint);
+        }
+
+        public static T GetService()
+        {
+            IClientChannel clientChannel = (IClientChannel)ChannelFactory.CreateChannel();
+            return(T)clientChannel;
+        }
+
+        public static TResult Execute<TResult>(Func<T, TResult> action)
+        {
+            IClientChannel clientChannel = (IClientChannel)ChannelFactory.CreateChannel();
+            TResult result;
+
+            bool success = false;
+            try
+            {
+                result = action((T)clientChannel);
+                clientChannel.Close();
+                success = true;
+            }
+            finally
+            {
+                if (!success)
+                {
+                    clientChannel.Abort();
+                }
+            }
+            return result;
+        }
+    }
+}
